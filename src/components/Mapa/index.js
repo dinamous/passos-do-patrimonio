05c9f9fc6663d03e48be1100e947a8e1fef41112
matheus simoes <matheusaugustosimoes@hotmail.com>
2021-11-12 17:07:50 -0300

    pequenos ajustes

--- src/components/Mapa/index.js
+++ src/components/Mapa/index.js
@@ -1,23 +1,21 @@
+import React from "react";
+import "./style.css";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import locais from "../../dados.json";
+import marker from "../../images/marker.png";
+import L from "leaflet";
 
-import React from 'react';
-import './style.css';
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
-import locais from '../../dados.json'
-import marker from '../../images/marker.png'
-import L from 'leaflet';
-
-import { LocalContext } from '../../providers/Local';
+import { LocalContext } from "../../providers/Local";
 
 function Mapa() {
+  const { origem, zoom } = React.useContext(LocalContext);
 
-  const {origem,zoom} = React.useContext(LocalContext)
+  console.log(origem);
 
-  console.log(origem)
-
-  function GetIcon(_iconSize ) {
+  function GetIcon(_iconSize) {
     return L.icon({
       iconUrl: marker,
-      iconSize: [55,55],
+      iconSize: [55, 55],
     });
   }
 
@@ -26,25 +24,30 @@ function Mapa() {
     map.setView(center, zoom);
     return null;
   }
- 
-  return ( 
-     <MapContainer center={origem} zoom={zoom} scrollWheelZoom={true}>
-       <ChangeView center={origem} zoom={zoom} /> 
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
 
-          {locais.map(local => (
-             <Marker  key={local.id} position={[local.GPS.lat, local.GPS.long]} icon={GetIcon(30)}>
-             <Popup>
-              {local.local}
-             </Popup>
-           </Marker>
-          ))}
+  return (
+    <MapContainer center={origem} zoom={zoom} scrollWheelZoom={true}>
+      <ChangeView center={origem} zoom={zoom} />
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
 
-       
-      </MapContainer>
+      {locais.map((local) => (
+        <Marker
+          key={local.id}
+          position={[local.GPS.lat, local.GPS.long]}
+          icon={GetIcon(30)}
+        >
+          <Popup>
+            <div className="centro">
+              <h3>{local.local}</h3>
+              <img src={local.fotos[0]} alt="" style={{ width: "200px" }} />
+            </div>
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
   );
 }
 
--- src/providers/Local.js
+++ src/providers/Local.js
@@ -5,7 +5,7 @@ export const LocalContext = React.createContext({});
 export const LocalProvider = (props) =>{
   const [origem, setOrigem] = useState([-20.7204, -46.6101])
   const [lugar, setLugar] = useState({})
-  const [zoom, setZoom] = useState(16)
+  const [zoom, setZoom] = useState(18)
 
   return (
     <LocalContext.Provider value={{origem,setOrigem,
