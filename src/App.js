8292e803ba6614deccfff97487a36efaa8e45c70
matheus simoes <matheusaugustosimoes@hotmail.com>
2021-11-12 21:09:13 -0300

    ajuste no preloader e no marker

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Main from './components/Main';
+import Preloader from './components/Preloader';
 
 function App() {
   return (
     <div className="App">
+      <Preloader/>
      <Main/>
     </div>
   );
--- src/components/Preloader/index.js
+++ src/components/Preloader/index.js
@@ -1,32 +1,27 @@
-
-import './style.css';
-import logo from '../../images/Group 3.png'
-import logoNome from '../../images/PASSOS DO PATRIMÔNIO.svg'
-import { Col, Container, Row } from 'react-bootstrap';
-import { useEffect, useState } from 'react';
+import "./style.css";
+import logo from "../../images/Group 3.png";
+import logoNome from "../../images/PASSOS DO PATRIMÔNIO.svg";
+import { useEffect, useState } from "react";
+import passos from "../../images/brasao-rodape.png";
 
 function Preloader() {
-  const [visivel, setVisivel] = useState(true)
+  const [visivel, setVisivel] = useState(true);
 
   useEffect(() => {
-    
-    setTimeout(()=>{
-      setVisivel(false)
-     }, 3000)
-  },[])
-
+    setTimeout(() => {
+      setVisivel(false);
+    }, 2000);
+  }, []);
 
   return (
-      <Container fluid className={!visivel?'fadeOut':null}>
-        <Row>
-          <Col>
-          <div className="Preloader">
-            <img src={logo} alt="Logo da aplicação Passos do Patrimônio" />
-            <img src={logoNome} alt="Passos do Patrimônio" />
-          </div>
-          </Col>
-        </Row>
-      </Container>
+    <div className={!visivel ? "Preloader fadeOut" : "Preloader"}>
+      <img src={logo} alt="Logo da aplicação Passos do Patrimônio" />
+      <img src={logoNome} alt="Passos do Patrimônio" />
+      <div className="linha">
+        <p>por Itamar Machado</p>
+        <img src={passos} alt="Brasão da cidade de Passos" />
+      </div>
+    </div>
   );
 }
 
